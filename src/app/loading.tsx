7cd396c05c9d740e0228c3a81d7bafa9401bf889
Mahdi Ramezani <mahdi@Mahdis-MacBook-Air.local>
2025-12-05 00:12:32 +0330

    add loading file

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { BeatLoader } from "react-spinners";
+
 export default function Loading() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h2 className="font-dana-medium dark:text-white">در حال بارگذاری...</h2>
+      <div className="flex items-center justify-center flex-col gap-2">
+        <BeatLoader color="#a6eb4e" size={20} />
+        <h2 className="font-dana-medium dark:text-white">در حال بارگذاری</h2>
+      </div>
     </div>
   );
 }
