09a87466f75d90bc8b4fb320ee0ccb48daa0f9ee
Mahdi Ramezani <mahdi@Mahdis-MacBook-Air.local>
2025-12-05 00:03:47 +0330

    add loading file

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loading() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h2 className="font-dana-medium dark:text-white">در حال بارگذاری...</h2>
+    </div>
+  );
+}
